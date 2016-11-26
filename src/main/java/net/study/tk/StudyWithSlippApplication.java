97b113256942886f18b4a34d90b25f4f21d3581e
Taekyung Lee <terry@terryui-MacBook-Pro.local>
2016-11-26 15:41:14 +0900

    initial commit

---
+++ src/main/java/net/study/tk/StudyWithSlippApplication.java
@@ -0,0 +1,12 @@
+package net.study.tk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWithSlippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyWithSlippApplication.class, args);
+	}
+}
